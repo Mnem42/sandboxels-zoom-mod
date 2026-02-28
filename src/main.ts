1638d73f09fb473b635f92a2a0a9c948e463b84f
Mnem42 <Mnem424242@gmail.com>
2026-02-28 11:24:41 +0000

    clean: blindly follow eslint

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import Handler from "./handler"
 import Patcher from "./patcher"
 
 dependOn("betterSettings.js", () => {
-    let on_change = {cb: () => {}}
+    const on_change = {cb: () => {}}
     const settings_manager = new CustomSettingsManager(on_change)
 
     runAfterLoad(() => {
         const patcher = new Patcher(settings_manager)
-        const handler = new Handler(settings_manager, patcher)
+        new Handler(settings_manager, patcher)
+        
         on_change.cb = () => patcher.update_from_settings()
     })
 }, true);
